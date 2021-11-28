8c362d18305df8adf857d6601d4d2d30d20376ab
Rakesh Arunachalam <rakesh@thoughtbot.com>
2021-11-28 15:50:03 +0000

    Remove remix logo and add my name

---
+++ app/components/Nav/NavHeader.tsx
@@ -0,0 +1,14 @@
+import { Link } from "remix";
+
+function NavHeader() {
+  return (
+    <Link
+      className="text-xl font-bold text-yellow-800 dark:text-yellow-200 md:text-xl hover:text-gray-700 dark:hover:text-gray-300"
+      to="/"
+    >
+      Rakesh Arun
+    </Link>
+  );
+}
+
+export default NavHeader;
